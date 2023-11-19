03e5e7151f0e87e0e02f333d190c900e87ed423a
Madhav-25 <bypureddymadhav@gmail.com>
2023-11-19 09:22:48 +0530

    Added sign up logic.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, flash, redirect, url_for, session
 from flask_cors import CORS
 import numpy as np
 import mediapipe as mp
@@ -6,8 +6,12 @@ import cv2
 import PoseModule as pm
 import matplotlib.pyplot as plt
 import base64
+from pymongo import MongoClient
+from datetime import datetime
+import uuid
 
 app = Flask(__name__)
+app.secret_key = "pose-detection"
 CORS(app)
 
 @app.route('/')
@@ -15,11 +19,28 @@ def home():
     return render_template('login.html')
 
 
+@app.route('/home')
+def home_page():
+    return render_template("home.html")
+
+
 @app.route('/login', methods=['POST'])
 def login():
     username = request.form['username']
     password = request.form['password']
-    return render_template('home.html')
+    user = user_collection.find_one({'username': username, 'password': password})
+    print("123456")
+    print(user)
+    if user:
+        session["username"] = username
+        session["password"] = password
+        return redirect(url_for('home'))
+    return jsonify({'error': 'User not found, Please Sign up'})
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 @app.route('/process_images', methods=['POST'])
 def process_images():
@@ -51,7 +72,7 @@ def process_images():
         comparison_image_rgb = detector.findPose(comparison_image_rgb)
         comparison_landmarks = detector.getPosition(comparison_image_rgb)
 
-        threshold = 150
+        threshold = 300
         # Compare pose landmarks
         if main_landmarks and comparison_landmarks:
             similarity_score = calculate_similarity(main_landmarks, comparison_landmarks)
@@ -94,6 +115,51 @@ def image_to_base64(image):
     image_base64 = base64.b64encode(buffer).decode()
     return image_base64
 
+@app.route('/save_user_data', methods=['POST'])
+def save_user_data():
+    data = request.form
+
+    # Validate the incoming data
+    required_fields = ['username', 'password', 'email']
+    for field in required_fields:
+        if field not in data:
+            return jsonify({"error": f"Missing {field} in the request"}), 400
+
+    username = data['username']
+    password = data['password']
+    email = data['email']
+
+    # Save user data to MongoDB
+    display_picture = request.files.get('displayImage')
+    display_picture_base64 = None
+
+     # Check if a user with the same username already exists
+    existing_user = user_collection.find_one({"username": username})
+    if existing_user:
+        return jsonify({"error": f"User with username '{username}' already exists"}), 400
+
+    if display_picture:
+        display_picture_base64 = base64.b64encode(display_picture.read()).decode('utf-8')
+
+    user_data = {
+        "user_id": str(uuid.uuid1()),
+        "username": username,
+        "password": password,  # Note: In a production environment, hash the password before storing it
+        "email": email,
+        "registration_date": datetime.now(),
+        "display_picture": display_picture_base64
+    }
+    user_id = user_collection.insert_one(user_data).inserted_id
+
+    return jsonify({"message": f"User with ID {user_id} registered successfully"}), 200
+
 
 if __name__ == '__main__':
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["pose-detection"]
+    # Create User collection
+    user_collection = db["User"]
+
+    # Create ExercisePoseAssessment collection
+    exercise_pose_collection = db["ExercisePoseAssessment"]
     app.run()
